53e978c83195c368971f7e8ba67f6ba922ace454
Lucas Rodrigues <65752473+spek-dev@users.noreply.github.com>
2023-08-22 15:20:03 -0300

    first commit

--- main.py
+++ main.py
@@ -5,24 +5,29 @@ def send_product_to_api(product_data, headers):
     url = "https://api.instabuy.com.br/store/products"
     response = requests.put(url, json=product_data, headers=headers)
 
+api_key = "Mq1EWAXiHwraLIQgfq4stmUxKiM6VpC5Xd9o3wuX1Go"
+
 headers = {
-    "Authorization": "Bearer Mq1EWAXiHwraLIQgfq4stmUxKiM6VpC5Xd9o3wuX1Go"
+    "api-key": api_key
 }
 
 with open('products.csv', 'r', newline='', encoding='utf-8') as csvfile:
     reader = csv.DictReader(csvfile, delimiter=';')
     for row in reader:
+        price = float(row['Preço regular'].replace(',', '.'))
+        formatted_price = f"R${price:.2f}"  # Format to Brazilian currency
+        
         product_data = {
             "id": row['Código interno'],
             "name": row['Nome'],
             "brand": row['Código de barras'],
             "pc": [],
-            "valid_price": float(row['Preço regular'].replace(',', '.')),
+            "valid_price": price,
         }
         send_product_to_api(product_data, headers)
         
         print("Produto ID:", product_data["id"])
         print("Nome:", product_data["name"])
         print("Marca:", product_data["brand"])
-        print("Preço:", product_data["valid_price"])
+        print("Preço:", formatted_price)
         print('-' * 40)
